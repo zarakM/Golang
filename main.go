1e24297e4c209ffa10360d46d17fc5137c263ac9
zarakM <mughalzarak@gmail.com>
2020-06-25 19:04:25 +0500

    type added

--- main.go
+++ main.go
@@ -9,3 +9,10 @@ import (
 
     "github.com/gorilla/mux"
 )
+
+type Article struct {
+    Id      string    `json:"Id"`
+    Title   string `json:"Title"`
+    Desc    string `json:"desc"`
+    Content string `json:"content"`
+}
