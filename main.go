8f3e56370d867ea728d0d4cd47d6ff57009b2e64
zarakM <mughalzarak@gmail.com>
2020-06-25 19:05:20 +0500

    get articles functions

--- main.go
+++ main.go
@@ -16,3 +16,26 @@ type Article struct {
     Desc    string `json:"desc"`
     Content string `json:"content"`
 }
+
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("Endpoint Hit: returnAllArticles")
+    json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    key := vars["id"]
+
+    for _, article := range Articles {
+        if article.Id == key {
+            json.NewEncoder(w).Encode(article)
+        }
+    }
+}
