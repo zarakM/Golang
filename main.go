3430c39844bac6dbf98a0123fd92e62c501e0556
zarakM <mughalzarak@gmail.com>
2020-06-25 21:28:06 +0500

    All arithmetic operation included

--- main.go
+++ main.go
@@ -35,6 +35,69 @@ func add(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Result is %v", result)
 }
 
+func subtract(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	one := vars["one"]
+	two := vars["two"]
+
+	var (
+		first_operand  int
+		second_operand int
+	)
+	if i, err := strconv.Atoi(one); err == nil {
+		first_operand = i
+	}
+
+	if i, err := strconv.Atoi(two); err == nil {
+		second_operand = i
+	}
+
+	result := strconv.Itoa(first_operand - second_operand)
+	fmt.Fprintf(w, "Result is %v", result)
+}
+
+func multiply(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	one := vars["one"]
+	two := vars["two"]
+
+	var (
+		first_operand  int
+		second_operand int
+	)
+	if i, err := strconv.Atoi(one); err == nil {
+		first_operand = i
+	}
+
+	if i, err := strconv.Atoi(two); err == nil {
+		second_operand = i
+	}
+
+	result := strconv.Itoa(first_operand * second_operand)
+	fmt.Fprintf(w, "Result is %v", result)
+}
+
+func divide(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	one := vars["one"]
+	two := vars["two"]
+
+	var (
+		first_operand  int
+		second_operand int
+	)
+	if i, err := strconv.Atoi(one); err == nil {
+		first_operand = i
+	}
+
+	if i, err := strconv.Atoi(two); err == nil {
+		second_operand = i
+	}
+
+	result := strconv.Itoa(first_operand / second_operand)
+	fmt.Fprintf(w, "Result is %v", result)
+}
+
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/add/{one}/{two}", add)
